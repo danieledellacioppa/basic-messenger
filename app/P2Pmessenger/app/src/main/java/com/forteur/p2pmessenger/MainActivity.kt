6199df9562d900ff0fabbd66b8113fc9ecdfa0f6
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2025-01-19 19:45:41 +0000

    initial empty hello world

---
+++ app/P2Pmessenger/app/src/main/java/com/forteur/p2pmessenger/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.forteur.p2pmessenger
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.forteur.p2pmessenger.ui.theme.P2PmessengerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            P2PmessengerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    P2PmessengerTheme {
+        Greeting("Android")
+    }
+}
